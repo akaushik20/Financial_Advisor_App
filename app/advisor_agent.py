0774a9c96f67f184c6132aaae7aa799c1473c32a
Arpita Kaushik <arpita.k20@gmail.com>
2025-06-22 19:42:49 -0700

    added prompt

--- app/advisor_agent.py
+++ app/advisor_agent.py
@@ -13,6 +13,18 @@ openai_apikey=os.getenv("OPENAI_API_KEY")
 client=OpenAI(api_key = openai_apikey)
 
 def advisor_agent(state: AdvisorState) -> AdvisorState:
+    prompt = f'''You are a financial advisor. The user has input amount: enter amount, 
+    risk appetite: enter risk appetite, and additional comments: enter additional comments.
+    Here are the invetment options for risk appetite listed below:
+
+    1. High Risk: Stocks, Cryptocurrencies, Commodities
+    2. Medium Risk: Mutual Funds, ETFs, Bonds
+    3. Low Risk: Fixed Deposits, Savings Accounts, Government Bonds
+
+    Depending on the above information can you summarize the market conditions and provide investment advice?
+      
+    Based on the user inputs, provide investment advice.'''
+
     response = client.chat.completions.create(
         model="gpt-4",
         messages=[
